6076b6bbc6971e39c9aaa4f055e161a0341621f3
Harry <328418417@qq.com>
2015-09-10 16:45:14 +0800

    first commit

---
+++ Example/Pods/Target Support Files/HDDemo/HDDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HDDemo : NSObject
+@end
+@implementation PodsDummy_HDDemo
+@end
---
+++ Example/Pods/Target Support Files/HDDemo/HDDemo-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double HDDemoVersionNumber;
+FOUNDATION_EXPORT const unsigned char HDDemoVersionString[];
+
