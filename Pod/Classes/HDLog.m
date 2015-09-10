15067a4f30b354cc8aea1a8a792062e126ac5265
Harry <328418417@qq.com>
2015-09-10 17:06:17 +0800

    change

---
+++ Pod/Classes/HDLog.m
@@ -0,0 +1,17 @@
+//
+//  HDLog.m
+//  HDDemo
+//
+//  Created by Harry on 15/9/10.
+//  Copyright (c) 2015年 Harry. All rights reserved.
+//
+
+#import "HDLog.h"
+
+@implementation HDLog
+
++ (void)HDlog{
+    NSLog(@"HDlog");
+}
+
+@end
